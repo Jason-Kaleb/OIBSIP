c468310ccb7493c2b041696606e463fa91f4c2c4
Jason <kyleskaleb56@gmail.com>
2024-04-02 18:00:25 +0200

    updated java files

--- taskno-2/guessingGame/src/guessinggame/GuessingGame.java
+++ taskno-2/guessingGame/src/guessinggame/GuessingGame.java
@@ -4,6 +4,7 @@ public class GuessingGame
 {
     public static void main(String[] args)
     {
-        
+        gameScreen Game = new gameScreen();
+        Game.setVisible(true);
     } 
 }
--- taskno-2/guessingGame/src/guessinggame/gameScreen.java
+++ taskno-2/guessingGame/src/guessinggame/gameScreen.java
@@ -1,13 +1,16 @@
+
 package guessinggame;
 
 import java.util.Random;
 import javax.swing.JOptionPane;
+
 
 public class gameScreen extends javax.swing.JFrame
 {
     public gameScreen()
     {
         initComponents();
+        restartBtn.setVisible(false);
     }
 
     /**
@@ -25,12 +28,23 @@ public class gameScreen extends javax.swing.JFrame
         userInput = new javax.swing.JTextField();
         updateText = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
+        restartBtn = new javax.swing.JButton();
         score = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        chances = new javax.swing.JLabel();
+        exitBtn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Guess The Number");
 
         guessBtn.setText("Guess");
+        guessBtn.setFocusable(false);
+        guessBtn.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                guessBtnMouseClicked(evt);
+            }
+        });
         guessBtn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 guessBtnActionPerformed(evt);
@@ -49,7 +63,6 @@ public class gameScreen extends javax.swing.JFrame
             }
         });
 
-        updateText.setText("jLabel3");
         updateText.addContainerListener(new java.awt.event.ContainerAdapter() {
             public void componentRemoved(java.awt.event.ContainerEvent evt) {
                 updateTextComponentRemoved(evt);
@@ -58,36 +71,69 @@ public class gameScreen extends javax.swing.JFrame
 
         jLabel4.setText("Score:");
 
-        score.setText("100");
+        restartBtn.setText("Restart");
+        restartBtn.setFocusable(false);
+        restartBtn.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                restartBtnMouseClicked(evt);
+            }
+        });
+        restartBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                restartBtnActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setText("Chances:");
+
+        chances.setText("5");
+
+        exitBtn.setText("Exit");
+        exitBtn.setFocusable(false);
+        exitBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                exitBtnActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 487, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(31, 31, 31)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(guessBtn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(guessBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 320, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(userInput, javax.swing.GroupLayout.PREFERRED_SIZE, 205, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(updateText, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(updateText, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(restartBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 320, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(score, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                .addContainerGap(21, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(score, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(chances, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(exitBtn, javax.swing.GroupLayout.DEFAULT_SIZE, 136, Short.MAX_VALUE))))
+                .addGap(15, 15, 15))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(32, 32, 32)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel1)
                 .addGap(36, 36, 36)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -95,11 +141,20 @@ public class gameScreen extends javax.swing.JFrame
                     .addComponent(userInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(score, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addComponent(updateText, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(guessBtn)
-                .addContainerGap(18, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(updateText, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel3)
+                        .addComponent(jLabel5)
+                        .addComponent(chances, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(guessBtn)
+                    .addComponent(exitBtn))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(restartBtn)
+                .addGap(23, 23, 23))
         );
 
         pack();
@@ -115,54 +170,96 @@ public class gameScreen extends javax.swing.JFrame
     }//GEN-LAST:event_userInputActionPerformed
 
     private void guessBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_guessBtnActionPerformed
-       
-        String userNumber = userInput.getText();
-        int userNumberConverted = Integer.parseInt(userNumber);
+
+    }//GEN-LAST:event_guessBtnActionPerformed
+
+    private void guessBtnMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_guessBtnMouseClicked
+     
+        int rounds = Integer.parseInt(chances.getText());
+
         Random random = new Random();
         int randomNumber = random.nextInt(100) + 1;
-        int rounds = 5;
-        
-        if (!(userNumber.isEmpty()))
+        int userScore = 100;
+        String userNumber = userInput.getText();
+        int userNumberConverted;
+
+        try 
         {
-            while (rounds != 0)
+            userNumberConverted = Integer.parseInt(userNumber);
+
+            if (!(userNumber.isEmpty() || userNumberConverted <= 0 || userNumberConverted > 100))
             {
-                updateText.setText("Hello");
-                rounds--;
-                 if (rounds == 0)
-            {
-                
-                guessBtn.setVisible(false);
-                updateText.setText("Game over");
+                if (userNumberConverted == randomNumber)
+                {
+                    userScore += 900;
+                    guessBtn.setVisible(false);
+                    restartBtn.setVisible(true);
+                    updateText.setText("YOU WIN!!!, Correct Number: " + randomNumber);
+                }
+                else if (userNumberConverted < randomNumber)
+                {
+                    updateText.setText("Guess again but Higher, Correct Number: " + randomNumber);
+                    userScore -= 100;
+                    rounds--;
+                }
+                else
+                {
+                    updateText.setText("Guess again but Lower, Correct Number: " + randomNumber);
+                    userScore -= 100;
+                    rounds--;
+                }
+                score.setText(Integer.toString(userScore));
+                chances.setText(Integer.toString(rounds));
             }
-            }
-           
         }
-        else
+        catch (NumberFormatException ex)
         {
-            JOptionPane.showMessageDialog(this, "Please enter a number");
+            JOptionPane.showMessageDialog(this, "Please enter a number between 1-100");
         }
-    }//GEN-LAST:event_guessBtnActionPerformed
+        if (rounds == 0)
+        {
+            guessBtn.setVisible(false);
+            restartBtn.setVisible(true);
+            updateText.setText("Game Over, Correct Number: " + randomNumber);
+        }
+    }//GEN-LAST:event_guessBtnMouseClicked
+
+    private void restartBtnMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_restartBtnMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_restartBtnMouseClicked
+
+    private void restartBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_restartBtnActionPerformed
+        dispose();
+        gameScreen newGame = new gameScreen();
+        newGame.setVisible(true);
+    }//GEN-LAST:event_restartBtnActionPerformed
+
+    private void exitBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitBtnActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_exitBtnActionPerformed
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) 
-    {
-      
-        java.awt.EventQueue.invokeLater(new Runnable() 
-        {
-            public void run()
-            {
-                new gameScreen().setVisible(true);
-            }
-        });
-    }
+    public static void main(String args[]) {
+
+    java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            new gameScreen().setVisible(true);
+        }
+    });
+}
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel chances;
+    private javax.swing.JButton exitBtn;
     private javax.swing.JButton guessBtn;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JButton restartBtn;
     private javax.swing.JLabel score;
     private javax.swing.JLabel updateText;
     private javax.swing.JTextField userInput;
